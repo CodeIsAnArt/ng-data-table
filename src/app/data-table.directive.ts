54ad9db531d228aeb2271f0f23042179de78ca52
droidlover <amanjain92@gmail.com>
2018-07-04 09:41:56 +0530

    Fixed header implementation

--- src/app/data-table.directive.ts
+++ src/app/data-table.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, Input} from '@angular/core';
 
 @Directive({
   selector: '[appDataTable]'
 })
 export class DataTableDirective {
+@Input() thead;
+@Input() trow;
+  constructor(private el: ElementRef) {console.log(this.thead);
 
-  constructor() { }
+    console.log(this.el);}
+
 
 }
